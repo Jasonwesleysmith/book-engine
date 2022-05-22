62dee6a5c9a61fd545ef56d622e4061db907cdde
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-05-22 14:02:59 -0500

    added app get to server

--- server/server.js
+++ server/server.js
@@ -10,6 +10,19 @@ const {authMiddleware} = require('./utils/auth')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,7 +31,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
